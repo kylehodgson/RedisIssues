e1bd765af9e598afed17843630433b4402df396d
Kyle Hodgson <kyle.hodgson@gmail.com>
2014-09-07 13:01:02 -0400

    initial checkin

---
+++ RedisBugs/RedisBugs/RedisAutoWireProof.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Funq;
+using NUnit.Framework;
+using ServiceStack;
+using ServiceStack.Redis;
+
+namespace RedisBugs
+{
+    [TestFixture]
+    public class RedisAutoWireProof
+    {
+        private ServiceHost _host;
+
+        [TestFixtureSetUp] public void FixtureSetup()
+        {
+            _host = new ServiceHost();
+        }
+
+        [TestFixtureTearDown] public void FixtureTeardown()
+        {
+            _host.Dispose();
+        }
+
+        [Test]
+        public void ShouldBeAbleToAutoWireIRedisClient()
+        {
+            _host.Init();
+            var redis = _host.Resolve<IRedisClient>();
+            Assert.IsNotNull(redis);
+        }
+    }
+
+    public class TestService : Service
+    { }
+
+    public class ServiceHost : AppHostBase
+    {
+        public ServiceHost() : base("Test Service", typeof(TestService).Assembly)
+        { }
+
+        public override void Configure(Container container)
+        {
+            container.RegisterAutoWiredAs<RedisClient, IRedisClient>();
+        }
+    }
+}
---
+++ RedisBugs/RedisBugs/SerializationBugProof.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ServiceStack.Redis;
+
+namespace RedisBugs
+{
+    [TestFixture]
+    public class SerializationBugProof
+    {
+        [Test]
+        public static void ShouldSerializeDateEntitiesInListsWithTimeZoneOffset()
+        {
+            var date = DateTime.Now;
+            using (var redis = new RedisClient())
+            {
+                redis.FlushDb();
+
+                var list = redis.As<DateTime>().Lists["listId"];
+                list.Add(date);             // silently drops timezone offset (0400)
+                var dateFromList = list[0]; // fetch the incorrectly stored date
+                Assert.AreEqual(date.ToLocalTime(), dateFromList.ToLocalTime());
+
+            }
+        }
+    }
+}
